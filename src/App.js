1e3f3cdcd136b9c9f53972ee9f1b922b096ea670
kknyapple <kknyapple@gmail.com>
2022-05-20 15:50:58 +0900

    map으로 div 사용 줄이기

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     "강남 우동 맛집",
     "파이썬독학",
   ]);
-  const [like, setLike] = useState(0);
+  const [like, setLike] = useState([0, 0, 0]);
   const [modal, setModal] = useState(false);
 
   return (
@@ -15,35 +15,38 @@ function App() {
       <div className="black-nav">
         <div>React blog</div>
       </div>
-      <div className="list">
-        <h4
-          onClick={() => {
-            {
-              //modal === true ? setModal(false) : setModal(true);
-              setModal(!modal);
-            }
-          }}
-        >
-          {title[0]}
-          <span
-            onClick={() => {
-              setLike(like + 1);
-            }}
-          >
-            👍
-          </span>
-          {like}
-        </h4>
-        <p>5월 18일 발행</p>
-        <hr></hr>
-        <h4>{title[1]}</h4>
-        <p>5월 18일 발행</p>
-        <hr></hr>
-        <h4>{title[2]}</h4>
-        <p>5월 18일 발행</p>
-        <hr></hr>
-      </div>
-      {modal == true ? <Modal></Modal> : null}
+      {title.map(function (a, i) {
+        return (
+          <div className="list">
+            <h4>
+              <h4
+                onClick={() => {
+                  {
+                    //modal === true ? setModal(false) : setModal(true);
+                    setModal(!modal);
+                  }
+                }}
+              >
+                {title[i]}
+              </h4>
+              <span
+                onClick={() => {
+                  let copy = [...like];
+                  copy[i] = copy[i] + 1;
+                  setLike(copy);
+                }}
+              >
+                👍
+              </span>
+              {like[i]}
+              <p>5월 18일 발행</p>
+            </h4>
+            <hr></hr>
+          </div>
+        );
+      })}
+
+      {modal === true ? <Modal></Modal> : null}
     </div>
   );
 }
