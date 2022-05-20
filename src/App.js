eddc849df98ba88c55e58548a880f1c8cb49758a
kknyapple <kknyapple@gmail.com>
2022-05-20 15:24:21 +0900

    component 만들기

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     "파이썬독학",
   ]);
   const [like, setLike] = useState(0);
+  const [modal, setModal] = useState(false);
 
   return (
     <div className="App">
@@ -15,7 +16,14 @@ function App() {
         <div>React blog</div>
       </div>
       <div className="list">
-        <h4>
+        <h4
+          onClick={() => {
+            {
+              //modal === true ? setModal(false) : setModal(true);
+              setModal(!modal);
+            }
+          }}
+        >
           {title[0]}
           <span
             onClick={() => {
@@ -35,17 +43,21 @@ function App() {
         <p>5월 18일 발행</p>
         <hr></hr>
       </div>
-      <button
-        onClick={() => {
-          let copy = [...title];
-          copy[0] = "여자코트 추천";
-          setTitle(copy);
-        }}
-      >
-        수정
-      </button>
+      {modal == true ? <Modal></Modal> : null}
     </div>
   );
 }
+
+const Modal = () => {
+  return (
+    <>
+      <div className="modal">
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+    </>
+  );
+};
 
 export default App;
